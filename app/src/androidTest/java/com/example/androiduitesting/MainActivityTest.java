a81e5dadeaa6c1748d2d068df7c482b522472542
Hareem400 <hareemxkhann@gmail.com>
2026-03-10 23:54:18 +0500

    Lab done!

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -3,8 +3,6 @@ package com.example.androiduitesting;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -14,6 +12,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -26,13 +25,17 @@ import org.junit.runner.RunWith;
 @LargeTest
 public class MainActivityTest {
     @Rule
-    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<MainActivity>(MainActivity.class);
 
     @Test
     public void testAddCity(){
+        // Click on Add City button
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(replaceText("Edmonton"), closeSoftKeyboard());
+        // Type "Edmonton" in the editText
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        // Click on Confirm
         onView(withId(R.id.button_confirm)).perform(click());
+        // Check if text "Edmonton" is matched with any of the text displayed on the screen
         onView(withText("Edmonton")).check(matches(isDisplayed()));
     }
 
@@ -40,13 +43,13 @@ public class MainActivityTest {
     public void testClearCity(){
         // Add first city to the list
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(replaceText("Edmonton"), closeSoftKeyboard());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
-
+        //Add another city to the list
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(replaceText("Vancouver"), closeSoftKeyboard());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Vancouver"));
         onView(withId(R.id.button_confirm)).perform(click());
-
+        //Clear the list
         onView(withId(R.id.button_clear)).perform(click());
         onView(withText("Edmonton")).check(doesNotExist());
         onView(withText("Vancouver")).check(doesNotExist());
@@ -54,56 +57,53 @@ public class MainActivityTest {
 
     @Test
     public void testListView(){
+        // Add a city
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(replaceText("Edmonton"), closeSoftKeyboard());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
-
-        onData(is(instanceOf(String.class)))
-            .inAdapterView(withId(R.id.city_list))
-            .atPosition(0)
-            .check(matches(withText("Edmonton")));
+        // Check if in the Adapter view (given id of that adapter view),there is a data
+        // (which is an instance of String) located at position zero.
+        // If this data matches the text we provided then Voila! Our test should pass
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list
+        )).atPosition(0).check(matches((withText("Edmonton"))));
     }
 
     @Test
     public void testShowActivitySwitch() {
+        // Add a city
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(replaceText("Edmonton"), closeSoftKeyboard());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
-
-        onData(is(instanceOf(String.class)))
-            .inAdapterView(withId(R.id.city_list))
-            .atPosition(0)
-            .perform(click());
-
+        // Click on the city in the list
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
+        // Check whether the activity correctly switched by checking if textView_cityName is displayed
         onView(withId(R.id.textView_cityName)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testCityNameConsistency() {
+        // Add a city
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(replaceText("Edmonton"), closeSoftKeyboard());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
-
-        onData(is(instanceOf(String.class)))
-            .inAdapterView(withId(R.id.city_list))
-            .atPosition(0)
-            .perform(click());
-
+        // Click on the city in the list
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
+        // Test whether the city name is consistent
         onView(withId(R.id.textView_cityName)).check(matches(withText("Edmonton")));
     }
 
     @Test
     public void testBackButton() {
+        // Add a city
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(replaceText("Edmonton"), closeSoftKeyboard());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
-
-        onData(is(instanceOf(String.class)))
-            .inAdapterView(withId(R.id.city_list))
-            .atPosition(0)
-            .perform(click());
-
+        // Click on the city in the list
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
+        // Test the back button
         onView(withId(R.id.button_back)).perform(click());
+        // Check if we are back in MainActivity
         onView(withId(R.id.city_list)).check(matches(isDisplayed()));
     }
+
 }
